324549126e62afc10464b7f047a47a7493dd228b
JunHo Seo <junhoseo@korea.ac.kr>
2024-10-03 04:33:35 +0000

    feat: Add context length.

--- decision_transformer.py
+++ decision_transformer.py
@@ -27,16 +27,16 @@ class CausalAttentionBlock(nnx.Module):
         )
 
     def __call__(self, x):
-        B, T, C = x.shape
+        T, C = x.shape
 
-        causal_mask = nnx.make_causal_mask(jnp.zeros((B, T)))
+        causal_mask = nnx.make_causal_mask(jnp.zeros((T,)))
 
         att = self.ln_1(x)
-        q, k, v = jnp.split(self.kqv(att), 3, axis=2)
-        k = k.reshape(B, T, self.num_heads, C // self.num_heads)
-        q = q.reshape(B, T, self.num_heads, C // self.num_heads)
-        v = v.reshape(B, T, self.num_heads, C // self.num_heads)
-        att = nnx.dot_product_attention(q, k, v, mask=causal_mask).reshape(B, T, C)
+        q, k, v = jnp.split(self.kqv(att), 3, axis=1)
+        k = k.reshape(T, self.num_heads, C // self.num_heads)
+        q = q.reshape(T, self.num_heads, C // self.num_heads)
+        v = v.reshape(T, self.num_heads, C // self.num_heads)
+        att = nnx.dot_product_attention(q, k, v, mask=causal_mask).reshape(T, C)
         x = x + att
         x = x + self.mlp(self.ln_2(x))
         return x
@@ -77,7 +77,7 @@ class DecisionGPT(nnx.Module):
         )
 
     def __call__(self, obss, actions, return_to_gos, timesteps):
-        B, T = obss.shape[:2]
+        T = obss.shape[0]
 
         obs_embeddings = self.embed_obs(obss)
         act_embeddings = self.embed_act(actions)
@@ -89,32 +89,36 @@ class DecisionGPT(nnx.Module):
         return_embeddings += timestep_embeddings
 
         seqs = jnp.stack([return_embeddings, obs_embeddings, act_embeddings], axis=1)
-        seqs = jnp.permute_dims(seqs, (0, 2, 1, 3)).reshape(B, 3 * T, self.emb_dim)
+        seqs = jnp.permute_dims(seqs, (1, 0, 2)).reshape(3 * T, self.emb_dim)
 
         x = self.transformer(seqs)
         x = self.ln(x)
 
-        x = x.reshape(B, T, 3, self.emb_dim)
-        x = jnp.permute_dims(x, (0, 2, 1, 3))
-        return self.action_head(x[:, 1])
+        x = x.reshape(T, 3, self.emb_dim)
+        x = jnp.permute_dims(x, (1, 0, 2))
+        return self.action_head(x[1])
 
 
-def loss_fn(model: DecisionGPT, batch):
-    actions = model(
-        batch["observations"],
-        batch["actions"],
-        batch["return_to_gos"],
-        batch["timesteps"],
-    )
-    loss = ((actions - batch["actions"]) ** 2).mean()
+@nnx.vmap
+def loss_fn(model: DecisionGPT, states, actions, return_to_gos, timesteps):
+    pred_actions = model(states, actions, return_to_gos, timesteps)
+    loss = ((pred_actions - actions) ** 2).mean()
     return loss
 
 
 @nnx.jit
 def train_step(
-    model: DecisionGPT, optimizer: nnx.Optimizer, metrics: nnx.MultiMetric, batch
+    model: DecisionGPT,
+    optimizer: nnx.Optimizer,
+    metrics: nnx.MultiMetric,
+    states,
+    actions,
+    return_to_gos,
+    timesteps,
 ):
-    loss, grads = nnx.value_and_grad(loss_fn)(model, batch)
+    loss, grads = nnx.value_and_grad(loss_fn)(
+        model, states, actions, return_to_gos, timesteps
+    )
     metrics.update(values=loss)
     optimizer.update(grads)
 
@@ -131,28 +135,41 @@ def rollout(
     eval_episodes: int = 10,
     eval_steps: int = 1000,
     options: dict | None = None,
+    context_len: int = 20,
 ) -> list[float]:
     cumulative_rewards = []
     for _ in range(eval_episodes):
         cumulative_reward = 0.0
         state = env.reset(options=options)
 
-        states = jnp.asarray([state])
-        actions = jnp.zeros((0, model.act_size))
-        return_to_gos = jnp.array([[target_return]])
-        timesteps = jnp.array([[0]])
+        states = [state]
+        actions = []
+        return_to_gos = [target_return]
+        timesteps = [0]
 
         for t in range(eval_steps):
-            actions = jnp.concat([actions, jnp.zeros((1, model.act_size))], axis=0)
-            action = get_action(model, states, actions, return_to_gos, timesteps)
-            actions.at[-1].set(action)
+            actions.append(jnp.zeros(env.action_space.shape[0]))
+
+            states_input = jnp.asarray(states[-context_len:])
+            actions_input = jnp.asarray(actions[-context_len:])
+            return_to_gos_input = jnp.asarray(return_to_gos[-context_len:])[..., None]
+            timesteps_input = jnp.asarray(timesteps[-context_len:])
+
+            # It will cause recompilation on first <context_len> steps.
+            action = get_action(
+                model,
+                states_input,
+                actions_input,
+                return_to_gos_input,
+                timesteps_input,
+            )
+            action = action[-1]
+            actions[-1] = action
 
             state, reward, done, _ = env.step(action)
-            states = jnp.concat([states, jnp.asarray([state])], axis=0)
-            return_to_gos = jnp.concat(
-                [return_to_gos, jnp.array([[return_to_gos[-1] - reward]])]
-            )
-            timesteps = jnp.concat([timesteps, jnp.full((1, 1), t)])
+            states.append(state)
+            return_to_gos.append(return_to_gos[-1] - reward)
+            timesteps.append(t)
 
             cumulative_reward += reward
 
@@ -183,36 +200,39 @@ def main():
         rngs=nnx.Rngs(0),
     )
 
-    checkpoints.save_checkpoint("/workspaces/XAI606/models", nnx.state(model), step=0)
+    checkpoints.save_checkpoint(
+        "/workspaces/XAI606/models", nnx.state(model), step=0, overwrite=True
+    )
     metrics = nnx.metrics.Average()
     optimizer = nnx.Optimizer(model, optax.adam(3e-4))
 
-    rollout_cfg_type = namedtuple("RolloutConfig", ["eval"])
-    eval_cfg_type = namedtuple(
-        "EvalConfig", ["env_id", "dataset_id", "max_episode_steps", "seed"]
-    )
-    cfg = rollout_cfg_type(
-        eval=eval_cfg_type(
-            env_id="halfcheetah-medium-v2",
-            dataset_id="halfcheetah-medium-v2",
-            max_episode_steps=1000,
-            seed=0,
-        )
-    )
-
     batch_size = 16
+    context_len = 20
     key = jax.random.PRNGKey(0)
-    for step in range(1_000_000):
+    for step in range(100_000):
         key, sample_key = jax.random.split(key)
         rand_idx = jax.random.randint(sample_key, (batch_size,), 0, len(sequences))
         batch = {k: v[rand_idx] for k, v in dataset_seq.items()}
 
-        batch["return_to_gos"] = batch["rewards"].cumsum(axis=-1)[:, ::-1, None]
-        batch["rewards"] = batch["rewards"][:, :, None]
-        batch["timesteps"] = jnp.tile(
-            jnp.arange(start=0, stop=batch["rewards"].shape[1]), (batch_size, 1)
+        # Subsample sequences.
+        key, sample_key = jax.random.split(key)
+        T = jax.random.randint(sample_key, (batch_size,), context_len, 1000)
+        batch = {
+            k: v[t - context_len : t]
+            for k, v, t in zip(batch.keys(), batch.values(), T)
+        }
+
+        train_step(
+            model,
+            optimizer,
+            metrics,
+            batch["observations"],
+            batch["actions"],
+            batch["rewards"].cumsum(axis=-1)[:, ::-1, None],
+            jnp.tile(
+                jnp.arange(start=0, stop=batch["rewards"].shape[1]), (batch_size, 1)
+            ),
         )
-        train_step(model, optimizer, metrics, batch)
 
         if step > 0 and step % 1_000 == 0:
             loss = metrics.compute()
@@ -220,23 +240,34 @@ def main():
             print(f"Step {step}, Loss: {loss}")
 
         if step > 0 and step % 10_000 == 0:
-            model.eval()
-            cum_rews = rollout(
-                env,
-                model,
-                6000,
+            checkpoints.save_checkpoint(
+                "/workspaces/XAI606/models", nnx.state(model), step=step, overwrite=True
             )
-            print(f"Step {step}, Cumulative rewards: {cum_rews}")
+
+            model.eval()
+            perturbations = np.linspace(0.0, 100.0, 5)
+            for perturbations in perturbations:
+                cumulative_rewards = rollout(
+                    env,
+                    model,
+                    6000,
+                    options={"bshin_joint_stiffness": perturbations},
+                    context_len=context_len,
+                )
+                print(
+                    f"Perturbation: {perturbations:.2f} Return: {np.mean(cumulative_rewards):.2f}"
+                )
             model.train()
 
-            checkpoints.save_checkpoint(
-                "/workspaces/XAI606/models", nnx.state(model), step=step
-            )
-
-    perturbations = np.linspace(0.0, 100.0, 5)
+    model.eval()
+    perturbations = [180, 150, 130, 100, 80, 50, 0]
     for perturbations in perturbations:
         cumulative_rewards = rollout(
-            env, model, 6000, options={"bshin_joint_stiffness": perturbations}
+            env,
+            model,
+            6000,
+            options={"bshin_joint_stiffness": perturbations},
+            context_len=context_len,
         )
         print(
             f"Perturbation: {perturbations:.2f} Return: {np.mean(cumulative_rewards):.2f}"
